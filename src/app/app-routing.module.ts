e949af99d149ad136d5f90e16391c8e629a639ca
franckjm <jfpalaciosgrande@gmail.com>
2019-04-15 21:13:44 -0600

    Adding routes to it is app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { GetProductsComponent } from './get-products/get-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"products/add",
+    component:AddProductComponent
+  },
+  {
+    path:"products/edit/:id",
+    component:EditProductComponent
+  },
+  {
+    path:"products",
+    component:GetProductsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
